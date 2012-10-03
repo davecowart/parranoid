feda334d7c5f8854de5f2ddf2f775f75b599dcba
Dave Cowart <davecowart@gmail.com>
2012-10-03 01:12:58 -0500

    removed reference to localhost

--- public/javascripts/client/client.js
+++ public/javascripts/client/client.js
@@ -41,7 +41,7 @@ function catchup() {
 }
 
 $(function() {
-	var socket = io.connect('http://localhost');
+	var socket = io.connect('/');
 	clientViewModel = new ClientViewModel(socket);
 	ko.applyBindings(clientViewModel);
 	$('#rooms').tabs();
